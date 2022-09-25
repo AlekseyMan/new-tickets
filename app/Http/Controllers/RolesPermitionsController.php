3a9af01df2dbe083598e0a756ceae173af5b22de
AlekseyMan <51510268+AlekseyMan@users.noreply.github.com>
2022-09-25 15:21:50 +0700

    roles_permissions table added;

---
+++ app/Http/Controllers/RolesPermitionsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RolesPermitions;
+use Illuminate\Http\Request;
+
+class RolesPermitionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RolesPermitions  $rolesPermitions
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RolesPermitions $rolesPermitions)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RolesPermitions  $rolesPermitions
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RolesPermitions $rolesPermitions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RolesPermitions  $rolesPermitions
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RolesPermitions $rolesPermitions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RolesPermitions  $rolesPermitions
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RolesPermitions $rolesPermitions)
+    {
+        //
+    }
+}
