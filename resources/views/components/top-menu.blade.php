34c4906eea1d6466ba2b03267729e67754fc6bf1
AlekseyMan <51510268+AlekseyMan@users.noreply.github.com>
2022-10-23 14:54:45 +0700

    fix error with top menu

--- resources/views/components/top-menu.blade.php
+++ resources/views/components/top-menu.blade.php
@@ -1,8 +1,8 @@
 <div class="d-flex justify-content-center">
     <a href="/school" class="text-decoration-none"><button class="m-3 btn btn-secondary">Залы</button></a>
     <a href="/coaches" class="text-decoration-none"><button class="m-3 btn btn-secondary">Тренера</button></a>
-    <a href="/karateki?coach_id={{\Illuminate\Support\Facades\Auth::user()->profile->id}}" class="text-decoration-none"><button class="m-3 btn btn-secondary">Ученики</button></a>
-    <a href="/teams?coach_id={{\Illuminate\Support\Facades\Auth::user()->profile->id}}" class="text-decoration-none"><button class="m-3 btn btn-secondary">Команды</button></a>
+    <a href="/karateki?coach_id={{\Illuminate\Support\Facades\Auth::user()->profile->id ?? ""}}" class="text-decoration-none"><button class="m-3 btn btn-secondary">Ученики</button></a>
+    <a href="/teams?coach_id={{\Illuminate\Support\Facades\Auth::user()->profile->id ?? ""}}" class="text-decoration-none"><button class="m-3 btn btn-secondary">Команды</button></a>
     <a href="/settings" class="text-decoration-none"><button class="m-3 btn btn-secondary">Настройки</button></a>
     @auth
     <a href="{{ route('logout') }}" class="text-decoration-none"><button class="m-3 btn btn-secondary">Выйти</button></a>
