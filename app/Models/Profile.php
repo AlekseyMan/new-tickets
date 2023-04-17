92cb763553d64db5108b303381b5e78834911e44
AlekseyMan <51510268+AlekseyMan@users.noreply.github.com>
2023-04-17 08:02:45 +0700

    changed blocking balance from 15 to 5 seconds

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -150,7 +150,7 @@ class Profile extends Model
 
     public function isBlockedBalance(): bool
     {
-        if ($this->updated_at < now()->addSeconds(-15)) {
+        if ($this->updated_at < now()->addSeconds(-5)) {
             return false;
         }
         return true;
