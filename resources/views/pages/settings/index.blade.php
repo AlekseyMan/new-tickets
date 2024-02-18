a3a31d8d48dc0c547b52707fa7f36beb8d0cfc8b
AlekseyMan <vfyfkd@gmail.com>
2024-02-18 05:48:51 +0300

    from prod

--- resources/views/pages/settings/index.blade.php
+++ resources/views/pages/settings/index.blade.php
@@ -31,6 +31,7 @@
             <select name="year" id="year" class="form-select m-1">
                 <option value="2022">2022</option>
                 <option value="2023">2023</option>
+                <option value="2024">2024</option>
             </select>
         </div>
     </div>
